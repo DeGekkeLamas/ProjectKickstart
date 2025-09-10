3a399038c9138d17bd1d1d12aaa88153a840be96
DeGekkeLamas <karsvdtoorn@gmail.com>
2025-09-10 11:45:01 +0200

    cards folding out when hovering mostly functional

---
+++ ProjectKickoff/Assets/Scripts/CardUI/CardHandLayout.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using NaughtyAttributes;
+using UnityEngine.UI;
+using NUnit.Framework.Internal;
+
+public class CardHandLayout : MonoBehaviour
+{
+    public Vector2 rotationRange;
+    public Vector2 PlacementRange;
+
+    public List<GameObject> currentCards = new();
+
+    private void OnValidate()
+    {
+        rotationRange = new(rotationRange.x, Mathf.Max(rotationRange.x+1, rotationRange.y));
+        PlacementRange = new(PlacementRange.x, Mathf.Max(PlacementRange.x+1, PlacementRange.y));
+        //SetCardPositions();
+    }
+
+    [Button]
+    void SetCardPositions()
+    {
+        float canvasScale = transform.root.localScale.x;
+        if (currentCards.Count < 1)
+        {
+            Debug.LogWarning("There are no cards in the hand");
+            return;
+        }
+        for (int i = 0; i < currentCards.Count; i++)
+        {
+            // Place card
+            float inverseI = (1f/currentCards.Count) * i;
+            currentCards[i].transform.eulerAngles = new(0,0, Mathf.Lerp(rotationRange.x,rotationRange.y, inverseI));
+            currentCards[i].transform.position = this.transform.position + new Vector3(Mathf.Lerp(PlacementRange.x, PlacementRange.y, 1-inverseI), 0, 0) / canvasScale;
+            currentCards[i].GetComponent<Image>().color = Color.Lerp(Color.red, Color.green, inverseI);
+
+            // Correct hierarchy
+            currentCards[i].transform.SetSiblingIndex(0);
+        }
+    }
+}
---
+++ ProjectKickoff/Assets/Scripts/CardUI/FoldoutCard.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using UnityEngine;
+
+public class FoldoutCard : MonoBehaviour
+{
+    Vector3 originalScale;
+    Vector3 originalRotation;
+    public float maxScaleFactor = 3;
+    public int animationDuration = 30;
+
+    bool isFoldingOut;
+    bool isFoldingIn;
+    private void Awake()
+    {
+        originalScale = transform.localScale;
+    }
+    public void Foldout()
+    {
+            StartCoroutine(FoldoutAnimation());
+            Debug.Log("Folded out card");
+    }
+    IEnumerator FoldoutAnimation()
+    {
+        while (isFoldingIn)
+        {
+            yield return null;
+        }
+
+        isFoldingOut = true;
+        originalRotation = this.transform.localEulerAngles;
+        float inverseDuration = 1f / animationDuration;
+        float factor = Mathf.Pow(maxScaleFactor, inverseDuration);
+
+        for (int i = 0; i < animationDuration; i++)
+        {
+            this.transform.localScale *= factor;
+            this.transform.eulerAngles = Vector3.Lerp(originalRotation, !CloserToPlusSide(originalRotation.z, 0) ? 
+                Vector3.zero : new(0,0,360), inverseDuration * (i+1));
+
+            yield return new WaitForFixedUpdate();
+        }
+        isFoldingOut = false;
+    }
+    public void Foldin()
+    {
+            StartCoroutine(FoldinAnimation());
+            Debug.Log("Folded in card");
+    }
+    IEnumerator FoldinAnimation()
+    {
+        while (isFoldingOut)
+        {
+            yield return null;
+        }
+
+        isFoldingIn = true;
+        float inverseDuration = 1f / animationDuration;
+        float factor = Mathf.Pow(1f/maxScaleFactor, inverseDuration);
+
+        for (int i = 0; i < animationDuration; i++)
+        {
+            this.transform.localScale *= factor;
+            this.transform.eulerAngles = Vector3.Lerp(!CloserToPlusSide(originalRotation.z, 0) ?
+                Vector3.zero : new(0, 0, 360), originalRotation, inverseDuration * (i+1));
+
+            yield return new WaitForFixedUpdate();
+        }
+        isFoldingIn = false;
+    }
+
+    // Determines which direction is shorter for rotation
+    static bool CloserToPlusSide(float currentRotation, float destination)
+    {
+        float differenceUp = (360 + destination - currentRotation) % 360;
+        float differenceDown = (360 + currentRotation - destination) % 360;
+        return differenceDown > differenceUp;
+    }
+}
