a8653768331a13f7766e548a0e1aa3e673d84099
arthur <ajcvanittersum@gmail.com>
2025-09-16 17:22:11 +0200

    added camera zoom
    the player can now zoom in and out using their scroll wheel.

--- ProjectKickoff/Assets/Scripts/Player/LockCameraWithinBounds.cs
+++ ProjectKickoff/Assets/Scripts/Player/LockCameraWithinBounds.cs
@@ -4,14 +4,31 @@ public class LockCameraWithinBounds : MonoBehaviour
 {
     RectInt levelBounds;
     Camera cam;
+    float maxZoom;
+
     void Start()
     {
         levelBounds = LevelData.instance.levelBounds;
-        cam = this.GetComponent<Camera>();
+        cam = GetComponent<Camera>();
+
+        // Calculate maxZoom based on level bounds and screen aspect ratio
+        float aspectRatio = (float)Screen.width / Screen.height;
+        float maxZoomVertical = levelBounds.height / 2f;
+        float maxZoomHorizontal = levelBounds.width / (2f * aspectRatio);
+        maxZoom = Mathf.Min(maxZoomVertical, maxZoomHorizontal);
     }
 
     void Update()
     {
+        // zoom
+        float scrollInput = Input.GetAxis("Mouse ScrollWheel");
+        if (Mathf.Abs(scrollInput) > 0.01f)
+        {
+            cam.orthographicSize -= scrollInput;
+            cam.orthographicSize = Mathf.Clamp(cam.orthographicSize, 1, maxZoom);
+        }
+
+        //fix bounds
         float zoomW = cam.orthographicSize;
         float zoomH = cam.orthographicSize / ((float)Screen.height / Screen.width);
         transform.position = new(Mathf.Clamp(transform.parent.position.x, levelBounds.xMin + zoomH, levelBounds.xMax - zoomH)
