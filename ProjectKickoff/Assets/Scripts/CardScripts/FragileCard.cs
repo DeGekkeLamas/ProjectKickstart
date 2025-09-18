1829eb6e8346117f0a6987c03cb77346b57b428e
DeGekkeLamas <karsvdtoorn@gmail.com>
2025-09-18 14:53:13 +0200

    slightly changed fragile platform

--- ProjectKickoff/Assets/Scripts/CardScripts/FragileCard.cs
+++ ProjectKickoff/Assets/Scripts/CardScripts/FragileCard.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class FragileCard : CardBase
@@ -5,6 +6,15 @@ public class FragileCard : CardBase
     public Collider2D thisCardsCollider;
     protected override void ExitEffect(Collision2D collision)
     {
-        thisCardsCollider.enabled = false;
+        StartCoroutine(BreakCard());
+    }
+
+    /// <summary>
+    /// For if we want to animate it later
+    /// </summary>
+    IEnumerator BreakCard()
+    {
+        yield return null;
+        Destroy(this.gameObject);
     }
 }
