171cbf73d3a0d10c5593b2a58c0a771f714812f7
DeGekkeLamas <karsvdtoorn@gmail.com>
2025-09-19 14:24:56 +0200

    bounce platform now no longer bounces eternally preventing jumps, and now makes you jump higher but less intense than spring platform

--- ProjectKickoff/Assets/Scripts/CardScripts/SpringCard.cs
+++ ProjectKickoff/Assets/Scripts/CardScripts/SpringCard.cs
@@ -3,15 +3,16 @@ using UnityEngine;
 
 public class SpringCard : CardBase
 {
+    public float mulitplier = 2;
     
     protected override void EnterEffect(Collision2D collision)
     {
         PlayerController playerScript = collision.collider.gameObject.GetComponent<PlayerController>();
-        playerScript.jumpForce *= 2;
+        playerScript.jumpForce *= mulitplier;
     }
     protected override void ExitEffect(Collision2D collision)
     {
         PlayerController playerScript = collision.collider.gameObject.GetComponent<PlayerController>();
-        playerScript.jumpForce *= 0.5f;
+        playerScript.jumpForce *= 1f/mulitplier;
     }
 }
