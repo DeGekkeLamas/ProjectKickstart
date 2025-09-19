77fc3eaa3c1c9597ab6c71bea7462f01e0e182f8
DeGekkeLamas <karsvdtoorn@gmail.com>
2025-09-19 13:12:05 +0200

    background now moves separately

--- ProjectKickoff/Assets/Scripts/Player/BackgroundPlayerFollower.cs
+++ ProjectKickoff/Assets/Scripts/Player/BackgroundPlayerFollower.cs
@@ -4,18 +4,16 @@ using System.Collections.Generic;
 public class BackgroundPlayerFollower : MonoBehaviour
 {
     public float followSpeed = .5f;
-    public Vector3 spriteDimensions;
-    GameObject originalSprite;
+    Vector3 spriteDimensions;
+    public GameObject originalSprite;
 
     Dictionary<Vector3, GameObject> BGPiecesSpawned = new();
 
     private void Awake()
     {
-        originalSprite = transform.GetChild(0).gameObject;
         spriteDimensions = originalSprite.GetComponent<SpriteRenderer>().bounds.size;
-        Instantiate(originalSprite, originalSprite.transform.position + spriteDimensions, Quaternion.identity, this.transform);
     }
-    void Update()
+    void LateUpdate()
     {
         transform.position = Camera.main.transform.position * followSpeed;
 
@@ -24,22 +22,24 @@ public class BackgroundPlayerFollower : MonoBehaviour
         DebugExtension.DebugBounds(camBounds, Color.magenta);
 
         Vector3 pos = transform.position;
-        for(int x = 0; x < RoundToNearestSpritePos(camBounds.size + pos).x - pos.x / spriteDimensions.x; x++)
+        for(int x = 0; x < (RoundToNearestSpritePos(camBounds.size + pos).x - pos.x) / spriteDimensions.x + 1; x++)
         {
-            for (int y = 0; y < RoundToNearestSpritePos(camBounds.size + pos).y - pos.y / spriteDimensions.y; y++)
+            for (int y = 0; y < (RoundToNearestSpritePos(camBounds.size + pos).y - pos.y) / spriteDimensions.y + 1; y++)
             {
-                SpawnSprite( RoundToNearestSpritePos( new(camBounds.min.x + x * spriteDimensions.x, 
+                Vector3 spawnPos = RoundToNearestSpritePos(new(camBounds.min.x + x * spriteDimensions.x,
                     camBounds.min.y + y * spriteDimensions.y
-                    , 0) ) );
+                    , 0) );
+                DebugExtension.DebugWireSphere(spawnPos, Color.blue, 1);
+                SpawnSprite(spawnPos);
             }
         }
-        //Debug.Log($"{camBounds.size.x / spriteDimensions.x}, {camBounds.size.y / spriteDimensions.y}");
     }
 
     void SpawnSprite(Vector3 position)
     {
         if (BGPiecesSpawned.ContainsKey(position))
         {
+            Debug.Log("Did not spawn object");
             return;
         }
         BGPiecesSpawned[position] = Instantiate(originalSprite, position, Quaternion.identity, this.transform);
@@ -47,7 +47,6 @@ public class BackgroundPlayerFollower : MonoBehaviour
 
     Vector3 RoundToNearestSpritePos(Vector3 input)
     {
-        input -= this.transform.position;
         // Rounding
         input = MathTools.Vector3Divide(input, spriteDimensions);
         input = MathTools.Vector3Round(input);
