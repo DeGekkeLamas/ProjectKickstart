15aa443c1d02f649c0019291afb155c08eda8e41
DeGekkeLamas <karsvdtoorn@gmail.com>
2025-09-22 11:04:39 +0200

    placed coins in the scene and slightly increased some parts of the leveel

--- ProjectKickoff/Assets/Scripts/CoinCollect.cs
+++ ProjectKickoff/Assets/Scripts/CoinCollect.cs
@@ -2,9 +2,10 @@ using UnityEngine;
 
 public class CoinCollect : MonoBehaviour
 {
+    public int value = 1;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        GameManager.instance.UpdateCoinCount(GameManager.instance.collectedCoins + 1);
+        GameManager.instance.UpdateCoinCount(GameManager.instance.collectedCoins + value);
         Destroy(gameObject);
     }
 }
