f0ec767c637880bf446bc4a4021b66111744685f
arthur <ajcvanittersum@gmail.com>
2025-09-22 15:33:02 +0200

    Magnet rework
    magnet is stronger and now turns off when you touch it and when you leave it again it eventually turns on again

--- ProjectKickoff/Assets/Scripts/CardScripts/MagnetCard.cs
+++ ProjectKickoff/Assets/Scripts/CardScripts/MagnetCard.cs
@@ -1,19 +1,55 @@
-using System.ComponentModel;
+using System.Collections;
 using UnityEngine;
 
 public class MagnetCard : CardBase
 {
     public float gravity;
+    public float effectRadiuS;
+    public bool magnetic;
+    private Coroutine coroutine;
 
     protected override void UpdateEffect()
     {
-        if (PlayerController.instance == null) return;
+        if (!magnetic) return;
+        PlayerController player = PlayerController.instance;
+        if (player == null) return;
 
-        Vector2 playerPos = PlayerController.instance.transform.position;
+        Vector2 playerPos = player.transform.position;
         Vector2 platformPos = transform.position;
         Vector2 diff = platformPos - playerPos;
         float distance = diff.magnitude;
+
+        if (distance > effectRadiuS) return;
+
         Vector2 attraction = diff.normalized / (distance * distance) * gravity;
-        PlayerController.instance.DoMove(attraction * Time.deltaTime);
+        float attractionY = attraction.y;
+        float gravityY = Mathf.Abs(Physics2D.gravity.y);
+
+        if (attractionY < gravityY) attraction.y = 0;
+
+        Vector2 moveDelta = attraction * Time.deltaTime;
+        player.DoMove(moveDelta);
+    }
+
+    protected override void EnterEffect(Collision2D collision)
+    {
+        magnetic = false;
+        if (coroutine != null) StopCoroutine(coroutine);
+        coroutine = null;
+    }
+
+    protected override void ExitEffect(Collision2D collision)
+    {
+        if (coroutine == null)
+        {
+            coroutine = StartCoroutine(Remagnetize());
+        }
+        
+    }
+
+    private IEnumerator Remagnetize()
+    {
+        yield return new WaitForSeconds(0.5f);
+        magnetic = true;
     }
 }
