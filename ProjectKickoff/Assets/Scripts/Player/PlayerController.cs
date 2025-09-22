5e689ec7426d1992cec499f1548306c1fca0a915
arthur <ajcvanittersum@gmail.com>
2025-09-22 17:51:03 +0200

    fixed bug that made the spring card not work
    the bounce platform and spring platform share a script and that was causing problems. i solved it by testin for the physics material on the colider instead of the script

--- ProjectKickoff/Assets/Scripts/Player/PlayerController.cs
+++ ProjectKickoff/Assets/Scripts/Player/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     Collider2D _collider;
     Rigidbody2D _rigidbody;
+    PhysicsMaterial2D bouncyPhysicsMaterial;
     public static PlayerController instance;
     [HideInInspector] public Vector3 spawnPoint;
     [HideInInspector] public Vector3 worldSpawn;
@@ -67,7 +68,7 @@ public class PlayerController : MonoBehaviour
             this.transform.lossyScale - new Vector3(this.transform.lossyScale.x * .5f, 0)
             , 0, Vector3.down, .1f, ~LayerMask.GetMask("Player"));
         bool hitSomething = hit.collider != null;
-        if (hitSomething) if (hit.collider.TryGetComponent<SpringCard>(out SpringCard theSpringCard)) hitSomething = false;//so the player can't jump off of the trampoline card.
+        if (hitSomething) if (hit.collider.TryGetComponent<Collider2D>(out Collider2D theCollider)) if(theCollider.sharedMaterial.bounciness > 0.1f) hitSomething = false;//so the player can't jump off of the trampoline card.
         bool angleOutOfRange = Vector2.Dot(Vector2.up, hit.normal) <= 0;
 
         // Debug
