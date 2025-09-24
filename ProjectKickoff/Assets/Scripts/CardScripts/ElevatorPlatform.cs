58e8669de9b1cb2305c7279ba668c5408d784598
DeGekkeLamas <karsvdtoorn@gmail.com>
2025-09-24 15:49:53 +0200

    build update & moving platforms use timer variable now

--- ProjectKickoff/Assets/Scripts/CardScripts/ElevatorPlatform.cs
+++ ProjectKickoff/Assets/Scripts/CardScripts/ElevatorPlatform.cs
@@ -6,12 +6,19 @@ public class ElevatorPlatform : CardBase
     Vector3 startPos;
     public float movementRange = 2;
     public float speed = 1;
+
+    private float timer;
+    private void Update()
+    {
+        timer += Time.deltaTime;
+    }
+
     protected override void StartEffect()
     {
         startPos = transform.position;
     }
     protected override void UpdateEffect()
     {
-        transform.position = startPos + new Vector3(0, math.sin(Time.timeSinceLevelLoad * speed), 0) * movementRange;
+        transform.position = startPos + new Vector3(0, math.sin(timer * speed), 0) * movementRange;
     }
 }
--- ProjectKickoff/Assets/Scripts/CardScripts/MovingPlatform.cs
+++ ProjectKickoff/Assets/Scripts/CardScripts/MovingPlatform.cs
@@ -6,13 +6,20 @@ public class MovingPlatform : CardBase
     private Vector3 startPos;
     public float movementRange = 2;
     public float speed = 1;
+
+    private float timer;
+    private void Update()
+    {
+        timer += Time.deltaTime;
+    }
+
     protected override void StartEffect()
     {
         startPos = transform.position;
     }
     protected override void UpdateEffect()
     {
-        transform.position = startPos + new Vector3(math.sin(Time.timeSinceLevelLoad * speed), 0, 0) * movementRange;
+        transform.position = startPos + new Vector3(math.sin(timer * speed), 0, 0) * movementRange;
     }
     
     protected override void StayEffect(Collision2D collision)
