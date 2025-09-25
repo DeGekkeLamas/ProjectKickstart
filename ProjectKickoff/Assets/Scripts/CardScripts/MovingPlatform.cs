03a6fc4ea0ed76a61e79e17de5280a1787f445bb
DeGekkeLamas <karsvdtoorn@gmail.com>
2025-09-25 12:54:29 +0200

    yet another build update (fixed movingplatform)

--- ProjectKickoff/Assets/Scripts/CardScripts/MovingPlatform.cs
+++ ProjectKickoff/Assets/Scripts/CardScripts/MovingPlatform.cs
@@ -22,8 +22,8 @@ public class MovingPlatform : CardBase
     protected override void StayEffect(Collision2D collision)
     {
         PlayerController playerScript = collision.collider.gameObject.GetComponent<PlayerController>();
-        Vector3 oldPos = startPos + new Vector3(math.sin(Time.timeSinceLevelLoad - Time.deltaTime), 0, 0);
-        Vector3 currentPos = startPos + new Vector3(math.sin(Time.timeSinceLevelLoad), 0, 0);
+        Vector3 oldPos = startPos + new Vector3(math.sin(timer - Time.deltaTime), 0, 0);
+        Vector3 currentPos = startPos + new Vector3(math.sin(timer), 0, 0);
         Vector3 diffPos = currentPos - oldPos;
         playerScript.DoMove(diffPos * movementRange * speed);
     }
